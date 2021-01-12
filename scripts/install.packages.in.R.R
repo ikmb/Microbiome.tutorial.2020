e62a9038afc855ca8b526c810ebfef227488f4ea
Lucas <l.silva@ikmb.uni-kiel.de>
2021-01-12 13:15:50 +0100

    update

---
+++ scripts/install.packages.in.R.R
@@ -0,0 +1,35 @@
+R
+#
+install.packages("tidyverse")
+#
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("dada2", version = "3.11")
+#
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("phyloseq")
+#
+install.packages("vegan")
+#
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("microbiome")
+#
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("DESeq2")
+
+
+library(tidyverse) # Use of pipe and lots of functions for a better R coding
+library(phyloseq) # Because we are going to work with microbioe data using phyloseq object
+library(microbiome) # some useful functions
+library(DESeq2)
+library(dada2)
+library(vegan) # Many ecology related functions
+
+
+wget https://github.com/ikmb/Microbiome.tutorial.2020/archive/main.zip
+
+unzip main.zip -d ~/Desktop/microbiome
